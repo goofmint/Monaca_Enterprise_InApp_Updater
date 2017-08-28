27e6bb27783154e58221adb7dd94f82cf04a6532
Atsushi Nakatsugawa <atsushi@moongift.jp>
2017-08-28 10:22:36 +0900

    update

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
 var update  = require('./update.json')
+var fs = require('fs');
 
 /* GET home page. */
 router.post('/', function(req, res, next) {
   res.json(update);
+});
+
+router.post('/download', function(req, res, next) {
+  var buf = fs.readFileSync('111.zip');
+  res.send(buf, { 'Content-Type': 'application/zip' }, 200);
 });
 
 module.exports = router;
